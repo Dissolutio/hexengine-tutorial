442a3504986b78610f99fafa15690393c1a392da
John Moen <entity.john@gmail.com>
2021-03-25 17:01:19 -0500

    changed the Floor

--- src/Floor.ts
+++ src/Floor.ts
@@ -7,19 +7,20 @@ import {
   Physics,
   useDraw,
 } from "@hex-engine/2d";
+import Draggable from "./Draggable";
 
 export default function Floor(position: Vector) {
   useType(Floor);
 
   const geometry = useNewComponent(() =>
     Geometry({
-      shape: Polygon.rectangle(new Vector(200, 25)),
+      shape: Polygon.rectangle(new Vector(400, 10)),
       position: position.clone(),
     })
   );
 
   useNewComponent(() => Physics.Body(geometry, { isStatic: true }));
-
+  useNewComponent(() => Draggable(geometry));
   useDraw((context) => {
     context.fillStyle = "#ddd";
     geometry.shape.draw(context, "fill");
